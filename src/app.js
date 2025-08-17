a6670b2ee9900807c1cb30294bc61bfdf066b99a
Sunny Chan <schan6803@gmail.com>
2025-08-16 22:23:16 -0400

    adding a mongo db

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
-const express = require('express');
+import express from 'express';
+import dotenv from "dotenv";
+import { shorten } from './middleware/shorten.js';
+import { db } from '../db/mongoConn.js';
+
+dotenv.config()
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.json({"message": "test message"})
+app.get('/:id', (req, res) => {
+    res.send(`Get ID ${req.params.id}`)
 })
 
-app.post("/shorten", (req, res) => {
+app.post("/shorten", shorten, (req, res) => {
     res.sendStatus(201).json({"url": "Some url"})
 })
 
---
+++ src/middleware/shorten.js
@@ -0,0 +1,4 @@
+export const shorten = (req, res, next) => {
+    next()
+}
+
